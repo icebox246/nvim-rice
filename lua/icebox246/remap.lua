c788961cee1a15f6ac44aaae7d9f244450cd5c48
icebox246 <iceboxpoczta@gmail.com>
2023-02-22 11:35:41 +0100

    Add half-screen jumping cursor centering remaps

--- lua/icebox246/remap.lua
+++ lua/icebox246/remap.lua
@@ -18,3 +18,7 @@ vim.keymap.set('n', 'TT', function() vim.lsp.buf.format() end)
 
 -- Replace current word
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Half screen jumping
+vim.keymap.set('n', '<C-u>', "<C-u>zz")
+vim.keymap.set('n', '<C-d>', "<C-d>zz")
